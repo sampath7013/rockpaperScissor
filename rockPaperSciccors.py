158319d4a4379f609d0e6c267beabed9dcbb1571
sampath7013 <sampathkumarmuthyalapati@gmail.com>
2025-02-17 13:43:05 -0600

    Updated code

--- rockPaperSciccors.py
+++ rockPaperSciccors.py
@@ -1,16 +1,18 @@
 import random
 user_choice = int(input("What do you choose? '0' for rock, '1' for paper, '2' for scissors\n"))
-computer_choice = random.randint(0,2)
-print(f"Computer chose: {computer_choice}")
+
 if user_choice>2:
     print("You typed an invalid number. You lose.")
-elif computer_choice==user_choice:
-    print("It's a draw")
-elif computer_choice==0 and user_choice==2:
-    print("You lose")
-elif computer_choice==2 and user_choice==0:
-    print("You win")
-elif computer_choice>user_choice:
-    print("You lose")
-elif computer_choice<user_choice:
-    print("You win")   
+else:
+    computer_choice = random.randint(0,2)
+    print(f"Computer chose: {computer_choice}")
+    if computer_choice==user_choice:
+        print("It's a draw")
+    elif computer_choice==0 and user_choice==2:
+        print("You lose")
+    elif computer_choice==2 and user_choice==0:
+        print("You win")
+    elif computer_choice>user_choice:
+        print("You lose")
+    elif computer_choice<user_choice:
+        print("You win")    
